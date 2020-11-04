36101edc3b8b6090fe7355cb4af1b4781d9c8e09
Adam <aquanne@gmail.com>
2020-11-03 16:57:21 -0800

    Switched variables to help with debug

--- .travis/version.sh
+++ .travis/version.sh
@@ -11,8 +11,8 @@ fi
 
 # Sanity check, verify regex for version number and set varible
 if [[ ${current_version_arr[0]} =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-  echo "Setting NEW_VERSION_TAG to v${current_version_arr[0]}"
   export NEW_VERSION_TAG="v${current_version_arr[0]}"
+  echo "Setting NEW_VERSION_TAG to ${NEW_VERSION_TAG}"
 else
   echo "Version ${current_version_arr[0]} failed sanity check!"
   exit 1
@@ -20,8 +20,8 @@ fi
 
 # Verify multi-word description and set variable
 if (echo "${current_version_arr[1]}" | wc -w) > 4; then
-  echo "Setting NEW_VERSION_DESCRIPTION to: ${current_version_arr[1]}"
   export NEW_VERSION_DESCRIPTION=${current_version_arr[1]}
+  echo "Setting NEW_VERSION_DESCRIPTION to: ${NEW_VERSION_DESCRIPTION}"
 else
   echo "Description ${current_version_arr[1]} failed sanity check!"
   exit 1
